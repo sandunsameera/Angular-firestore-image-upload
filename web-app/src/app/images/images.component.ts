65eead780c5b12275912a711cb52cf86e3dcd22b
sandunsameera <sandunsameera25@gmail.com>
2019-12-06 22:23:42 +0530

    Init images component

---
+++ web-app/src/app/images/images.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-images',
+  templateUrl: './images.component.html',
+  styles: []
+})
+export class ImagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
